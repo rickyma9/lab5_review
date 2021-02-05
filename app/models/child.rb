112e0d0df06c5317b5217a9fc4d7cd0147bd6508
Ricky Ma <rickyma@andrew.cmu.edu>
2021-02-05 14:10:15 -0800

    added relationships and validations

--- app/models/child.rb
+++ app/models/child.rb
@@ -2,5 +2,10 @@ require 'simplecov'
 SimpleCov.start 'rails'
 
 class Child < ApplicationRecord
+  has_many :chores
+  has_many :tasks, through: :chores
+
+  validates_presence_of :first_name
+  validates_presence_of :last_name
     
 end
